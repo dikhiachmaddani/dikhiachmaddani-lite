55b4c9d9e1de1407ce0727e198c62794d06d8f5f
dikhiachmaddani <dikhiachmaddani@gmail.com>
2025-08-05 21:39:37 +0700

    feat(post): add new post

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,10 +19,11 @@ const projectCollection = defineCollection({
 		date: z.coerce.date(),
 		description: z.string(),
 		demo: z.string().nullable().default(null),
-		source: z.string(),
+		source: z.string().nullable().default(null),
 		type: z.enum(["personal", "assignment", "company"]),
 		stack: z.array(z.tuple([z.string(), z.string()])),
 		featured: z.boolean().default(false),
+		draft: z.boolean().optional().default(false),
 	}),
 });
 
